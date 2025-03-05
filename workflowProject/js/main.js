f9c8301342762309007300ec40db6ffee49d7da5
baldogy101 <baldogy01@gmail.com>
2025-03-05 22:26:37 +0800

    Delete workflowProject directory

--- workflowProject/js/main.js
+++
@@ -1,5 +0,0 @@
-document.addEventListener("DOMContentLoaded", function () {
-    loadComponent("components/header.html", "header");
-    loadComponent("components/main.html", "main-content");
-    loadComponent("components/footer.html", "footer");
-});
